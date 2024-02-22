21cf9818c8e031d23ae2d74eeb54a39989d3154c
Ajay-Dhangar <ajaydhangar49@gmail.com>
2024-02-22 16:46:44 +0530

    Added and content in JavaScript Web Dev

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 # FROM node:18-alpine
 
 # # The /app directory should act as the main application directory
-# WORKDIR /app
+# WORKDIR /site
 
 # # Copy the app package and package-lock.json file
 # COPY package*.json ./
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,40 +1,145 @@
-import clsx from 'clsx';
-import Heading from '@theme/Heading';
-import styles from './styles.module.css';
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
 
 const FeatureList = [
+  // {
+  //   title: 'Easy to Use',
+  //   Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+  //   description: (
+  //     <>
+  //       CodeHarbor is designed to be easy to learn and improve your coding skills. Start with the basics and learn at your own pace.
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: 'Focus on What Matters',
+  //   Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+  //   description: (
+  //     <>
+  //       CodeHarbor lets you focus on your coding skills, and we'll do the chores. Go ahead and learn the basics of coding.
+  //     </>
+  //   ),
+  // },
+  // {
+  //   title: 'Powered by React',
+  //   Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+  //   description: (
+  //     <>
+  //       CodeHarbor is built using React, a popular JavaScript library for building user interfaces. React makes it painless to create interactive UIs.
+  //     </>
+  //   ),
+  // },
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Blogging Platform",
+    Svg: require("@site/static/img/blogging.svg").default,
     description: (
       <>
-        CodeHarbor is designed to be easy to learn and improve your coding skills. Start with the basics and learn at your own pace.
+        CodeHarbor is a blogging platform for developers. Share your knowledge
+        and experience with the community.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Bug Fixes and Improvements",
+    Svg: require("@site/static/img/bug_fixing.svg").default,
     description: (
       <>
-        CodeHarbor lets you focus on your coding skills, and we'll do the chores. Go ahead and learn the basics of coding.
+        CodeHarbor is constantly being improved and updated. We are always open
+        to feedback and suggestions.
+      </>
+    ),
+  },
+
+  {
+    title: "YouTub Tutorial",
+    Svg: require("@site/static/img/youtube_tutorial.svg").default,
+    description: (
+      <>
+        CodeHarbor provides video tutorials on various topics. Learn from the
+        best and improve your coding skills.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Website Setup",
+    Svg: require("@site/static/img/website_setup.svg").default,
     description: (
       <>
-        CodeHarbor is built using React, a popular JavaScript library for building user interfaces. React makes it painless to create interactive UIs.
+        CodeHarbor provides a step-by-step guide to set up your website. Get
+        started with your own website today.
       </>
     ),
   },
+
+  {
+    title: "Web Development",
+    Svg: require("@site/static/img/web_development.svg").default,
+    description: (
+      <>
+        CodeHarbor provides tutorials on web development. Learn how to build
+        websites and web applications.
+      </>
+    ),
+  },
+  {
+    title: "Wall Post",
+    Svg: require("@site/static/img/wall_post.svg").default,
+    description: (
+      <>
+        CodeHarbor provides a wall post feature. Share your thoughts and ideas
+        with the community.
+      </>
+    ),
+  },
+
+  {
+    title: "Virtual Assistant",
+    Svg: require("@site/static/img/virtual_assistant.svg").default,
+    description: (
+      <>
+        CodeHarbor provides a virtual assistant to help you with your coding
+        problems. Get help from the best.
+      </>
+    ),
+  },
+
+  {
+    title: "Usability Testing",
+    Svg: require("@site/static/img/usability_testing.svg").default,
+    description: (
+      <>
+        CodeHarbor provides useability testing for your website. Get feedback
+        from the community and improve your website.
+      </>
+    ),
+  },
+
+  {
+    title: "Website",
+    Svg: require("@site/static/img/website.svg").default,
+    description: (
+      <>
+        CodeHarbor provides a website for developers. Share your knowledge and
+        experience with the community.
+      </>
+    ),
+  },
+
+  // {
+  //   title: 'Teaching',
+  //   Svg: require('@site/static/img/teaching.svg').default,
+  //   description: (
+  //     <>
+  //       CodeHarbor provides tutorials on teaching. Learn how to teach and share your knowledge with others.
+  //     </>
+  //   ),
+  // },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
