3e7ef8a7bbf66070868b32c49dbc607360cb1184
ajay-dhangar <ajaydhangar49@gmail.com>
2024-04-15 21:12:11 +0530

    Module 12: Advanced React Concepts added in React Course

---
+++ courses/react-js/advanced-level/advanced-react-concepts/Component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import withLogger from "./withLogger";
+
+const Component = ({ message }) => {
+  return <div>{message}</div>;
+};
+
+export default withLogger(Component);
