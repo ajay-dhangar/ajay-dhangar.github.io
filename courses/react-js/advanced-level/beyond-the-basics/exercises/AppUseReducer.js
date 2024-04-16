713804b7a7707265a29db39b4810e69110ebe2c2
Restyled.io <commits@restyled.io>
2024-04-16 17:57:23 +0000

    Restyled by clang-format

--- courses/react-js/advanced-level/beyond-the-basics/exercises/AppUseReducer.js
+++ courses/react-js/advanced-level/beyond-the-basics/exercises/AppUseReducer.js
@@ -1,17 +1,17 @@
-import React, { useReducer } from "react";
+import React, {useReducer} from "react";
 
 const initialState = {
-  count: 0,
+  count : 0,
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count - 1 };
-    default:
-      throw new Error();
+  case "increment":
+    return {count : state.count + 1};
+  case "decrement":
+    return {count : state.count - 1};
+  default:
+    throw new Error();
   }
 }
 
