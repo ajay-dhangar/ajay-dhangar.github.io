ae53d6b77180cab3ab14af5f703dcfe7e0ddf1f3
Restyled.io <commits@restyled.io>
2024-04-16 17:56:26 +0000

    Restyled by prettier

--- courses/react-js/advanced-level/beyond-the-basics/exercises/Counter.js
+++ courses/react-js/advanced-level/beyond-the-basics/exercises/Counter.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Counter() {
   const [count, setCount] = useState(0);
--- courses/react-js/advanced-level/beyond-the-basics/exercises/ExampleComponent.js
+++ courses/react-js/advanced-level/beyond-the-basics/exercises/ExampleComponent.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const ExampleComponent = () => {
   const [userData, setUserData] = useState(null);
