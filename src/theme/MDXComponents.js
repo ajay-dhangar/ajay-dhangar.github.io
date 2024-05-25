b8131224d2afe265f43646858d85e8212f0cb824
Restyled.io <commits@restyled.io>
2024-05-25 03:32:44 +0000

    Restyled by prettier

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,30 +1,26 @@
-import Head from '@docusaurus/Head';
-import BrowserWindow from '@site/src/components/BrowserWindow';
-import CollectionList from '@site/src/components/CollectionList';
-import Contributors from '@site/src/components/Contributors';
-import Courses from '@site/src/components/Courses';
-import ArrayVisualizations from
-    '@site/src/components/DSA/arrays/ArrayVisualizations';
-import BubbleSortVisualization from
-    '@site/src/components/DSA/arrays/BubbleSortVisualization';
-import InsertionSortVisualization from
-    '@site/src/components/DSA/arrays/InsertionSortVisualization';
-import SelectionSortVisualization from
-    '@site/src/components/DSA/arrays/SelectionSortVisualization';
-import File from '@site/src/components/File';
-import GiscusComponent from '@site/src/components/GiscusComponent';
-import Highlight from '@site/src/components/Highlight';
-import Lesson from '@site/src/components/Lesson';
-import SolutionAuthor from '@site/src/components/SolutionAuthor';
-import Table from '@site/src/components/Table';
-import TutorialAuthors from '@site/src/components/TutorialAuthors';
-import MDXComponents from '@theme-original/MDXComponents';
-import DocCardList from '@theme/DocCardList';
-import TabItem from '@theme/TabItem';
-import Tabs from '@theme/Tabs';
-import {FaReact} from 'react-icons/fa';
+import Head from "@docusaurus/Head";
+import BrowserWindow from "@site/src/components/BrowserWindow";
+import CollectionList from "@site/src/components/CollectionList";
+import Contributors from "@site/src/components/Contributors";
+import Courses from "@site/src/components/Courses";
+import ArrayVisualizations from "@site/src/components/DSA/arrays/ArrayVisualizations";
+import BubbleSortVisualization from "@site/src/components/DSA/arrays/BubbleSortVisualization";
+import InsertionSortVisualization from "@site/src/components/DSA/arrays/InsertionSortVisualization";
+import SelectionSortVisualization from "@site/src/components/DSA/arrays/SelectionSortVisualization";
+import File from "@site/src/components/File";
+import GiscusComponent from "@site/src/components/GiscusComponent";
+import Highlight from "@site/src/components/Highlight";
+import Lesson from "@site/src/components/Lesson";
+import SolutionAuthor from "@site/src/components/SolutionAuthor";
+import Table from "@site/src/components/Table";
+import TutorialAuthors from "@site/src/components/TutorialAuthors";
+import MDXComponents from "@theme-original/MDXComponents";
+import DocCardList from "@theme/DocCardList";
+import TabItem from "@theme/TabItem";
+import Tabs from "@theme/Tabs";
+import { FaReact } from "react-icons/fa";
 
-import YoutubeVideo from '../components/YoutubeVideo';
+import YoutubeVideo from "../components/YoutubeVideo";
 
 export default {
   // Re-use the default mapping
@@ -50,5 +46,5 @@ export default {
   File,
   YoutubeVideo,
   Lesson,
-  Head
+  Head,
 };
