6ef16573e3775475f87dbb230bc707ef32046828
Restyled.io <commits@restyled.io>
2024-05-25 03:32:33 +0000

    Restyled by clang-format

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,25 +1,30 @@
-import MDXComponents from '@theme-original/MDXComponents';
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
-import SolutionAuthor from '@site/src/components/SolutionAuthor';
-import TutorialAuthors from '@site/src/components/TutorialAuthors';
-import Contributors from '@site/src/components/Contributors';
-import Table from '@site/src/components/Table';
-import ArrayVisualizations from '@site/src/components/DSA/arrays/ArrayVisualizations';
-import SelectionSortVisualization from '@site/src/components/DSA/arrays/SelectionSortVisualization';
-import BubbleSortVisualization from '@site/src/components/DSA/arrays/BubbleSortVisualization';
-import BrowserWindow from '@site/src/components/BrowserWindow';
-import GiscusComponent from '@site/src/components/GiscusComponent';
-import CollectionList from '@site/src/components/CollectionList';
-import { FaReact } from 'react-icons/fa';
-import Courses from '@site/src/components/Courses';
-import DocCardList from '@theme/DocCardList';
-import Highlight from '@site/src/components/Highlight';
-import InsertionSortVisualization from '@site/src/components/DSA/arrays/InsertionSortVisualization';
-import File from '@site/src/components/File';
-import YoutubeVideo from '../components/YoutubeVideo';
-import Lesson from '@site/src/components/Lesson';
 import Head from '@docusaurus/Head';
+import BrowserWindow from '@site/src/components/BrowserWindow';
+import CollectionList from '@site/src/components/CollectionList';
+import Contributors from '@site/src/components/Contributors';
+import Courses from '@site/src/components/Courses';
+import ArrayVisualizations from
+    '@site/src/components/DSA/arrays/ArrayVisualizations';
+import BubbleSortVisualization from
+    '@site/src/components/DSA/arrays/BubbleSortVisualization';
+import InsertionSortVisualization from
+    '@site/src/components/DSA/arrays/InsertionSortVisualization';
+import SelectionSortVisualization from
+    '@site/src/components/DSA/arrays/SelectionSortVisualization';
+import File from '@site/src/components/File';
+import GiscusComponent from '@site/src/components/GiscusComponent';
+import Highlight from '@site/src/components/Highlight';
+import Lesson from '@site/src/components/Lesson';
+import SolutionAuthor from '@site/src/components/SolutionAuthor';
+import Table from '@site/src/components/Table';
+import TutorialAuthors from '@site/src/components/TutorialAuthors';
+import MDXComponents from '@theme-original/MDXComponents';
+import DocCardList from '@theme/DocCardList';
+import TabItem from '@theme/TabItem';
+import Tabs from '@theme/Tabs';
+import {FaReact} from 'react-icons/fa';
+
+import YoutubeVideo from '../components/YoutubeVideo';
 
 export default {
   // Re-use the default mapping
