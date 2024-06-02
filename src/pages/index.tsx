d3f2f377163b7e359347c7ae6523914c4115914c
Ajay Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2024-06-02 21:43:04 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -106,7 +106,7 @@ export default function Home() {
           <div>
             <div className={styles.topBannerDescription}>
               <p>
-                {/*                 I am a software engineer and a tech enthusiast. I love to learn
+                I am a software engineer and a tech enthusiast. I love to learn
                 and share my knowledge with others. I am passionate about web
                 development, open source, and building cool stuff. I am always
                 looking for new opportunities to learn and grow. I am currently
@@ -114,6 +114,8 @@ export default function Home() {
                 at CodeHarborHub. I love to help others learn and grow in their
                 careers. I am always looking for new opportunities to learn and
                 grow. */}
+
+                {/*
                 तत्र सोफ्टवेयर् इन्जिनियर् अहमस्मि, तथा प्रौद्योगिकी उत्सुकः
                 अहम्। अहं शिक्षितुं च अभ्यस्तुं च प्रियः। अहं वेब् विकासे, मुक्त
                 स्रोतौ, च अतिशयम् रचनायां प्रवृत्तः। सदैव नवानि अवसराणि
@@ -121,24 +123,23 @@ export default function Home() {
                 सोफ्टवेयर् इन्जिनियर् रूपेण कार्यम् करोमि। अहं च कोड् हार्बोर्
                 हब् नामके संस्थायां उपन्यासकः अस्मि। अहं परानुमोदितुं च
                 परिणामसाधयितुं च प्रियः। अहं सदैव नवानि अवसराणि अभ्यस्तुम् च
-                प्रवर्धितुम् च आवश्यकः।
+                प्रवर्धितुम् च आवश्यकः।                
                 <b>
                   {" "}
-                  <Link to="https://www.codeharborhub.live/">
-                    {/*                   Join CodeHarborHub */}
+                  <Link to="https://www.codeharborhub.live/">                    
                     कोड् हार्बोर् हब् सह योगः
                   </Link>{" "}
                 </b>{" "}
-                {/*                 and */}च{" "}
+                 च{" "}
                 <b>
-                  <Link to="https://www.codeharborhub.live/docs/">
-                    {/*                   start learning */}
+                  <Link to="https://www.codeharborhub.live/docs/">                    
                     कर्तुं आरम्भः
                   </Link>
                 </b>{" "}
-                {/*               today! */}, आजीविष्यताम् आरंभ आजीवया
+                , आजीविष्यताम् आरंभ आजीवया
                 अवलोकयस्व॥
-              </p>
+                */}
+                </p>
             </div>
           </div>
         </div>
