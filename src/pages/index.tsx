23a7219dbd5e7609594ed2fda96e6c98c86b1226
Ajay-Dhangar <ajaydhangar49@gmail.com>
2024-06-26 18:28:46 +0530

    ChatBot added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,12 +8,14 @@ import Heading from "@theme/Heading";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import Head from "@docusaurus/Head";
-// import Image from '@theme/IdealImage';
+import Image from "@theme/IdealImage";
 import Tweet from "@site/src/components/Tweet";
 import Tweets, { TweetItem } from "@site/src/data/tweets";
-// import Quotes from '@site/src/data/quotes';
+import Quotes from "@site/src/data/quotes";
 import styles from "./index.module.css";
 import AdComponent from "@site/src/components/AdComponent";
+import Chatbot from "@site/src/components/Chatbot";
+import { text } from "express";
 
 function TweetsSection() {
   const tweetColumns: TweetItem[][] = [[], [], []];
@@ -41,35 +43,35 @@ function TweetsSection() {
   );
 }
 
-// function QuotesSection() {
-//   return (
-//     <div className={clsx(styles.section)}>
-//       <div className="container">
-//         <div className="row">
-//           {Quotes.map((quote) => (
-//             <div className="col" key={quote.name}>
-//               <div className="avatar avatar--vertical margin-bottom--sm">
-//                 <Image
-//                   alt={quote.name}
-//                   className="avatar__photo avatar__photo--xl"
-//                   img={quote.thumbnail}
-//                   style={{overflow: 'hidden'}}
-//                 />
-//                 <div className="avatar__intro padding-top--sm">
-//                   <div className="avatar__name">{quote.name}</div>
-//                   <small className="avatar__subtitle">{quote.title}</small>
-//                 </div>
-//               </div>
-//               <p className="text--center text--italic padding-horiz--md">
-//                 {quote.text}
-//               </p>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
+function QuotesSection() {
+  return (
+    <div className={clsx(styles.section)}>
+      <div className="container">
+        <div className="row">
+          {Quotes.map((quote) => (
+            <div className="col" key={quote.name}>
+              <div className="avatar avatar--vertical margin-bottom--sm">
+                <Image
+                  alt={quote.name}
+                  className="avatar__photo avatar__photo--xl"
+                  img={quote.thumbnail}
+                  style={{ overflow: "hidden" }}
+                />
+                <div className="avatar__intro padding-top--sm">
+                  <div className="avatar__name">{quote.name}</div>
+                  <small className="avatar__subtitle">{quote.title}</small>
+                </div>
+              </div>
+              <p className="text--italic padding-horiz--md">
+                {quote.text}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -94,67 +96,77 @@ export default function Home() {
           src="https://pagead2.googlesyndication.com/pagead/managed/js/adsense/m202405220101/show_ads_impl_fy2021.js?bust=31083976"
         />
       </Head>
-      <div className={styles.topBanner}>
-        <div className={styles.topBannerTitle}>
-          {"🌟\xa0"}
-          <Link
-            to="/showcase/?tags=favorite"
-            className={styles.topBannerTitleText}
-          >
-            {"Check out the favorite projects in my showcase!"}
-          </Link>
-          {"\xa0🌟"}
-        </div>
-
-        <div
-          style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}
-        >
-          <div>
-            <div className={styles.topBannerDescription}>
-              <p>
-                {/*                 I am a software engineer and a tech enthusiast. I love to learn
-                and share my knowledge with others. I am passionate about web
-                development, open source, and building cool stuff. I am always
-                looking for new opportunities to learn and grow. I am currently
-                working as a software engineer at a startup. I am also a mentor
-                at CodeHarborHub. I love to help others learn and grow in their
-                careers. I am always looking for new opportunities to learn and
-                grow. */}
-                तत्र सोफ्टवेयर् इन्जिनियर् अहमस्मि, तथा प्रौद्योगिकी उत्सुकः
-                अहम्। अहं शिक्षितुं च अभ्यस्तुं च प्रियः। अहं वेब् विकासे, मुक्त
-                स्रोतौ, च अतिशयम् रचनायां प्रवृत्तः। सदैव नवानि अवसराणि
-                अभ्यस्तुम् च प्रवर्धितुम् च आवश्यकः। अहं सद्यः उपारंभिके
-                सोफ्टवेयर् इन्जिनियर् रूपेण कार्यम् करोमि। अहं च कोड् हार्बोर्
-                हब् नामके संस्थायां उपन्यासकः अस्मि। अहं परानुमोदितुं च
-                परिणामसाधयितुं च प्रियः। अहं सदैव नवानि अवसराणि अभ्यस्तुम् च
-                प्रवर्धितुम् च आवश्यकः।
-                <b>
-                  {" "}
-                  <Link to="https://www.codeharborhub.live/">
-                    {/*                   Join CodeHarborHub */}
-                    कोड् हार्बोर् हब् सह योगः
-                  </Link>{" "}
-                </b>{" "}
-                {/*                 and */}च{" "}
-                <b>
-                  <Link to="https://www.codeharborhub.live/docs/">
-                    {/*                   start learning */}
-                    कर्तुं आरम्भः
-                  </Link>
-                </b>{" "}
-                {/*               today! */}, आजीविष्यताम् आरंभ आजीवया
-                अवलोकयस्व॥
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <main>
         {" "}
+        <div className={styles.topBanner}>
+          <div className={styles.topBannerTitle}>
+            {"🌟\xa0"}
+            <Link
+              to="/showcase/?tags=favorite"
+              className={styles.topBannerTitleText}
+            >
+              {"Check out the favorite projects in my showcase!"}
+            </Link>
+            {"\xa0🌟"}
+          </div>
+
+          <div
+            style={{ display: "flex", alignItems: "center", flexWrap: "wrap" }}
+          >
+            <div>
+              <div
+                className={styles.topBannerDescription}
+                title="तत्र सोफ्टवेयर् इन्जिनियर् अहमस्मि, तथा प्रौद्योगिकी उत्सुकः
+                  अहम्। अहं शिक्षितुं च अभ्यस्तुं च प्रियः। अहं वेब् विकासे,
+                  मुक्त स्रोतौ, च अतिशयम् रचनायां प्रवृत्तः। सदैव नवानि अवसराणि
+                  अभ्यस्तुम् च प्रवर्धितुम् च आवश्यकः। अहं सद्यः उपारंभिके
+                  सोफ्टवेयर् इन्जिनियर् रूपेण कार्यम् करोमि। अहं च कोड् हार्बोर्
+                  हब् नामके संस्थायां उपन्यासकः अस्मि। अहं परानुमोदितुं च
+                  परिणामसाधयितुं च प्रियः। अहं सदैव नवानि अवसराणि अभ्यस्तुम् च
+                  प्रवर्धितुम् च आवश्यकः।"
+              >
+                <p>
+                  I am a software engineer and a tech enthusiast. I love to
+                  learn and share my knowledge with others. I am passionate
+                  about web development, open source, and building cool stuff. I
+                  am always looking for new opportunities to learn and grow. I
+                  am currently working as a software engineer at a startup. I am
+                  also a mentor at CodeHarborHub. I love to help others learn
+                  and grow in their careers. I am always looking for new
+                  opportunities to learn and grow.
+                  <b title="कोड् हार्बोर् हब् सह योगः">
+                    {" "}
+                    <Link to="https://www.codeharborhub.live/">
+                      Join CodeHarborHub
+                    </Link>{" "}
+                  </b>{" "}
+                  <span title="च">and</span>{" "}
+                  <b title="कर्तुं आरम्भः">
+                    <Link to="https://www.codeharborhub.live/docs/">
+                      start learning
+                    </Link>
+                  </b>{" "}
+                  <span
+                    title="आजीविष्यताम् आरंभ आजीवया
+                  अवलोकयस्व॥"
+                  >
+                    today!
+                  </span>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
         <AdComponent />
         <TweetsSection />
-        {/* <QuotesSection /> */}
+        <div style={{ margin: "10px 0" }}>
+          <Heading as="h3" style={{textAlign: "center"}}>
+            Quotes
+          </Heading>
+          <QuotesSection />
+        </div>
+        <Chatbot />
       </main>
     </Layout>
   );
