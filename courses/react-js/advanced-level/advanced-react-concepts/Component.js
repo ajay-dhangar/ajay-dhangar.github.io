9952441909b8a6de8537b782b651ab01a228c594
Ajay-Dhangar <ajaydhangar49@gmail.com>
2024-12-13 20:11:10 +0530

    modify

--- courses/react-js/advanced-level/advanced-react-concepts/Component.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import withLogger from "./withLogger";
-
-const Component = ({ message }) => {
-  return <div>{message}</div>;
-};
-
-export default withLogger(Component);
